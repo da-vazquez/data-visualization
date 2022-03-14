0e528990d69cb0adf2a503288b8f015b09bd79e2
Daniel Vazquez <da.vazquez728@gmail.com>
2022-03-14 01:03:05 -0700

    added labels to nodes

--- data-visualization/src/App.js
+++ data-visualization/src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
-//import ForceGraph2D from 'react-force-graph-2d'//2D
-import ForceGraph3D from 'react-force-graph-3d'//3D
+import ForceGraph2D from 'react-force-graph-2d'//2D
+//import ForceGraph3D from 'react-force-graph-3d'//3D
 import Axios from 'axios'
 
 import './App.css';
@@ -36,10 +36,8 @@ const App = () => {
   return (
     <div className="App">
       <button onClick={updateData}>Click for data</button>
-      <ForceGraph3D 
-        width={1500}
-        height={800}
-        nodeLabel="id"
+      <ForceGraph2D 
+        nodeLabel={x => x.innerHTML = "id"}
         nodeAutoColorBy="userId"
         nodeRelSize={6}
         graphData={graphData}
@@ -47,6 +45,28 @@ const App = () => {
         linkDirectionalParticleWidth={2}
         linkDirectionalParticles="value"
         linkDirectionalParticleSpeed={d => d.value * 0.001}
+        nodeCanvasObject={(node, ctx, globalScale) => {
+          const label = node.id;
+          const fontSize = 12/globalScale;
+          ctx.font = `${fontSize}px Sans-Serif`;
+          const textWidth = ctx.measureText(label).width;
+          const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+          ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+          ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.fillStyle = node.color;
+          ctx.fillText(label, node.x, node.y);
+
+          node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+        }}
+        nodePointerAreaPaint={(node, color, ctx) => {
+          ctx.fillStyle = color;
+          const bckgDimensions = node.__bckgDimensions;
+          bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+        }}
         onNodeDragEnd={node => {
           node.fx = node.x;
           node.fy = node.y;
